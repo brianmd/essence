6aac0c9ebcf17b3ba397eb0bd5a6dc5ec6313987
Brian Murphy-Dye <brian@murphydye.com>
2016-06-04 10:46:48 -0600

    Initial lein project.

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to essence
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/essence/core_test.clj
@@ -0,0 +1,7 @@
+(ns essence.core-test
+  (:require [clojure.test :refer :all]
+            [essence.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
