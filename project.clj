58ae233da12d039c6ddfc7d896766c675587ef0b
Brian Murphy-Dye <brian@murphydye.com>
2016-06-07 20:29:23 -0600

    Update to clojure 1.9.0-alpha5

--- project.clj
+++ project.clj
@@ -3,6 +3,6 @@
   :url "https://github.com/brianmd/essence.git"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]
+  :dependencies [[org.clojure/clojure "1.9.0-alpha5"]
                  [com.murphydye/utils "0.1.0"]]
   :source-paths ["src/clj" "src/cljs"])
